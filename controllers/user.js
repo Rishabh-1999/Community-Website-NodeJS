ef3ce22ec63e6a8efd3490bfb20e72655fd5cb16
Rishabh-1999 <rishabhanand33@gmail.com>
2020-01-25 12:41:22 +0530

    Bug Fix

--- controllers/user.js
+++ controllers/user.js
@@ -167,6 +167,7 @@ module.exports.changePassword = async function (req, res, next) {
                                 );
                             });
                         }
+                        else
                         res.send("false");
                     });
                 }
