e21b4f535c94d15123d2cf58e8a71d95178c6a1d
Rishabh Anand <38128234+Rishabh-1999@users.noreply.github.com>
2020-05-02 22:42:00 +0530

    Community Page updated UI and Code optimized

--- public/script/communitylists-js.js
+++ public/script/communitylists-js.js
@@ -6,66 +6,60 @@ $(document).ready(function () {
 
 function initaliseTable() {
   document.getElementById('can-create-community').innerHTML = "";
-  var s = "true"
+
   var xml = new XMLHttpRequest();
   xml.open("GET", "/communityTable/getArrayOwnCommunity");
   xml.setRequestHeader("Content-Type", "application/json");
   xml.addEventListener("load", function () {
     var data = JSON.parse(xml.responseText);
     for (var i = 0; i < data.length; i++)
-      addToDOM(data[i], s)
+      addToDOM(data[i], "true")
   })
   xml.send();
 }
 
 function initaliseTable1() {
-  var s = "false"
   var xml = new XMLHttpRequest();
   xml.open("GET", "/communityTable/getArrayOtherCommunity");
   xml.setRequestHeader("Content-Type", "application/json");
   xml.addEventListener("load", function () {
     var data = JSON.parse(xml.responseText);
     for (var i = 0; i < data.length; i++)
-      addToDOM(data[i], s)
+      addToDOM(data[i], "false")
   })
   xml.send();
 }
 
 function initaliseTable2() {
-  var s = "pending"
   var xml = new XMLHttpRequest();
   xml.open("GET", "/communityTable/getArrayOtherCommunityInvited");
   xml.setRequestHeader("Content-Type", "application/json");
   xml.addEventListener("load", function () {
     var data = JSON.parse(xml.responseText);
     for (var i = 0; i < data.length; i++)
-      addToDOM(data[i], s);
+      addToDOM(data[i], "pending");
   })
   xml.send();
 }
 
 function addToDOM(obj, s) {
   var div1 = document.createElement('div');
-  div1.setAttribute("class", "col-sm-12 col-xs-12 myCommunity community-div");
-  div1.setAttribute("style", "display: flex;");
+  div1.setAttribute("class", "d-flex col-sm-12 community-div");
   var div2 = document.createElement('div');
-  div2.setAttribute("class", "col-sm-1 col-xs-3")
-  div2.setAttribute("style", "padding:10px;flex:1;")
+  div2.setAttribute("class", "col-sm-2 d-flex p-4 align-middle")
   var img = document.createElement('img')
   img.setAttribute("src", obj.photoloc)
-  img.setAttribute("class", "cpic")
+  img.setAttribute("class", "community-pic")
   div2.appendChild(img)
   div1.appendChild(div2)
   var div3 = document.createElement('div')
-  div3.setAttribute("class", "col-sm-10 col-xs-7")
-  div3.setAttribute("style", "padding:20px;")
-  var p = document.createElement('p')
+  div3.setAttribute("class", "col-sm-8 p-5 align-middle")
+  var h4 = document.createElement('h4')
   var a2 = document.createElement('a');
   a2.setAttribute("class", "comnametxt")
   a2.href = "/communityTable/communityDicussion/" + obj._id;
   a2.innerHTML = obj.name
   var a3 = document.createElement('a')
-  a3.setAttribute("class", "comnametxt-user")
   if (s == "true") {
     a3.setAttribute("href", "/communityTable/" + obj._id);
     a3.innerHTML = " Members(" + obj.users.length + ")";
@@ -75,13 +69,13 @@ function addToDOM(obj, s) {
     a3.setAttribute("href", "/communityTable/communitymembers/" + obj._id);
     a3.innerHTML = " Request(" + obj.request.length + ")";
   }
-  p.appendChild(a2)
-  p.appendChild(a3)
-  div3.appendChild(p)
+  h4.appendChild(a2)
+  h4.appendChild(a3)
+  div3.appendChild(h4)
   div1.appendChild(div3)
   if (s == "true" || s == "pending") {
     var div4 = document.createElement('div')
-    div4.setAttribute("class", "col-sm-1 col-xs-2")
+    div4.setAttribute("class", "col-sm-2 p-4")
     var a4 = document.createElement('a')
     a4.setAttribute("class", "community-short-btn")
     if (s == "true")
@@ -91,12 +85,12 @@ function addToDOM(obj, s) {
     a4.setAttribute("style", "float:right;")
     var l1 = document.createElement('label')
     if (s == "true")
-      l1.setAttribute("class", "btn btn-success")
+      l1.setAttribute("class", "btn btn-lg btn-success")
     else
-      l1.setAttribute("class", "btn btn-danger")
+      l1.setAttribute("class", "btn btn-lg btn-danger")
     l1.setAttribute("style", "cursor:pointer !important;")
     var i1 = document.createElement('i')
-    i1.setAttribute("class", "fa fa-cogs")
+    i1.setAttribute("class", "fa fa-cogs fa-2x")
     l1.appendChild(i1)
     a4.appendChild(l1)
     div4.appendChild(a4)
