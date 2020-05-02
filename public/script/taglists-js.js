e1a29f8103cfbd73d6ec7083d5e8810b0ba54633
Rishabh Anand <38128234+Rishabh-1999@users.noreply.github.com>
2020-05-02 12:41:10 +0530

    Updated Tag Table UI and code optimization

--- public/script/taglists-js.js
+++ public/script/taglists-js.js
@@ -32,16 +32,17 @@ function initaliseTable() {
     ],
     "columnDefs": [{
       "targets": -1,
-
       "render": function (data, type, row, meta) {
-        data = '<button class=btn btn-sm" style="margin-top:0; margin-right:5px; background-color:#2D312C; color:#fff;" id="delete" onclick="deletetag()"><span class="fa fa-trash"></span></button><button class=btn btn-sm" style="margin-top:0; background-color:#2D312C; color:#fff;" id="editbtn" data-toggle="modal" data-target="#editModal" onclick=editTagModel()><span class="fa fa-edit"></span></button>';
+        data = '<button class="btn btn-lg mr-3" style="background-color:#2D312C; color:#fff;" id="delete" onclick="deletetag()"><span class="fa fa-trash"></span></button><button class="btn btn-lg" style="background-color:#2D312C; color:#fff;" id="editbtn" data-toggle="modal" data-target="#editModal" onclick=editTagModel()><span class="fa fa-edit"></span></button>';
         return data;
       }
     }],
   });
+
   $('#refresh').on('click', function () {
     table.ajax.reload(null, false);
   });
+
   $('#tagdiv').on('click', function () {
     table.ajax.reload(null, false);
   });
@@ -50,7 +51,6 @@ function initaliseTable() {
 function deletetag() {
   $(document).on("click", "#delete", function () {
     d = $(this).parent().parent()[0].children;
-    var u = d[0];
     var obj = new Object();
     obj.tagname = d[0].innerHTML;
     obj.createdby = d[1].innerHTML;
@@ -68,11 +68,9 @@ function deletetag() {
             request.addEventListener("load", function () {
               var d = request.responseText;
               if (d == "true") {
-                $("#errorMsg").text("Tag " + obj.tagname + " Deleted");
-                $('#errorModal').modal('show');
+                alert(obj.tagname + " Deleted")
               } else {
-                $("#errorMsg").text("Tag " + obj.tagname + " Not Deleted");
-                $('#errorModal').modal('show');
+                alert(obj.tagname + " Filed to Deleted");
               }
               table.ajax.reload(null, false);
             })
